046430b3e42d84b69a1fc5b7ba33d5e0b691554e
Hemal Varambhia <h.n.varambhia@gmail.com>
2012-12-07 18:48:19 +0000

    Added tests to check we correctly render NCBI query parameters for finished queries.
    Tests pass at this stage

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/FinishedQueriesActivityTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/FinishedQueriesActivityTest.java
@@ -27,6 +27,8 @@ public class FinishedQueriesActivityTest extends
 	
 	private BLASTQuery emblQuery;
 	
+	private BLASTQuery ncbiQuery;
+	
 	public FinishedQueriesActivityTest(
 			Class<FinishedQueriesActivity> activityClass) {
 		super(activityClass);
@@ -46,6 +48,12 @@ public class FinishedQueriesActivityTest extends
 		emblQuery.setJobIdentifier("EXAM123PLE_JOB_1D");		
 		emblQuery.setSearchParameter("email", "example@email.com");
 		saveQuery(emblQuery);
+		
+		ncbiQuery = new BLASTQuery("blastn", BLASTVendor.NCBI);
+		ncbiQuery.setStatus(BLASTQuery.Status.FINISHED);
+		ncbiQuery.setJobIdentifier("ANOTHER_EXAM123PLE_JOB_1D");		
+		saveQuery(ncbiQuery);
+		
 		solo = new Solo(getInstrumentation(), getActivity());
 		
 	}
@@ -73,7 +81,7 @@ public class FinishedQueriesActivityTest extends
 		
 	}
 
-	public void testThatTappingOptionToSeeParametersShowsParametersOfSelectedQuery(){
+	public void testThatTappingOptionToSeeParametersShowsParametersOfSelectedEMBLQuery(){
 		
 		
 		solo.waitForView(TextView.class);
@@ -128,8 +136,42 @@ public class FinishedQueriesActivityTest extends
 		
 		assertTrue("Should see the value of the e-mail parameter", valueOfEmailShown);
 		
+	}
+	
+	public void testThatTappingOptionToSeeParametersShowsParametersOfSelectedNCBIQuery(){
+		
+		
+		solo.waitForView(TextView.class);
+		
+		solo.clickLongInList(2);
+		
+		String viewParametersOption = getInstrumentation().getTargetContext().getResources().getString(R.string.view_query_parameters);
+		
+		solo.clickOnText(viewParametersOption);
+		
+		boolean checkJobIdIsShownAsTitle = solo.searchText(ncbiQuery.getJobIdentifier());
+		
+		assertTrue("Title is the job identifier", checkJobIdIsShownAsTitle);
+		
+		boolean programLabelShown = solo.searchText("Program");
+		
+		assertTrue("Should see the program label", programLabelShown);
+		
+		boolean valueOfProgramShown = solo.searchText(ncbiQuery.getBLASTProgram());
+		
+		assertTrue("Should see the value of the program parameter", valueOfProgramShown);
+		
+		boolean databaseLabelShown = solo.searchText("Database");
+		
+		assertTrue("Should see the database label", databaseLabelShown);
+		
+		boolean valueOfDatabaseShown = solo.searchText(ncbiQuery.getSearchParameter("database").getValue());
+		
+		assertTrue("Should see the value of the database parameter", valueOfDatabaseShown);
+		
 		
 	}
+	
 	
 	private void cleanDatabase(){
 		DatabaseHelper helper = new DatabaseHelper(getInstrumentation().getTargetContext());
