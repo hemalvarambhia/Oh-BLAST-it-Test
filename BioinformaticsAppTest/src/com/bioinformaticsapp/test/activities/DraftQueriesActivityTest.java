d71637022b4d934acdaa0ca57157399a2d0a3714
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-01-05 12:50:24 +0000

    Tests to confirm we can view parameters of a draft query

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/DraftQueriesActivityTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/DraftQueriesActivityTest.java
@@ -64,6 +64,26 @@ public class DraftQueriesActivityTest extends ActivityInstrumentationTestCase2<D
 		queryController.close();
 	}
 	
+	private long saveQuery(BLASTQuery query){
+		BLASTQueryController queryController = new BLASTQueryController(getInstrumentation().getTargetContext());
+		long primaryKey = queryController.save(query);
+		query.setPrimaryKeyId(primaryKey);
+		List<SearchParameter> parameters = query.getAllParameters();
+		List<SearchParameter> newSetOfParameters = new ArrayList<SearchParameter>();
+		SearchParameterController parametersControllers = new SearchParameterController(getInstrumentation().getTargetContext());
+		for(SearchParameter parameter: parameters){
+			parameter.setBlastQueryId(query.getPrimaryKey());
+			long parameterPrimaryKey = parametersControllers.save(parameter);
+			parameter.setPrimaryKey(parameterPrimaryKey);
+			newSetOfParameters.add(parameter);
+		}
+		
+		query.updateAllParameters(newSetOfParameters);
+		parametersControllers.close();
+		queryController.close();
+		return primaryKey;
+	}
+	
 	public void testWeCanViewAllDraftQueries(){
 		//This could be any query
 		createSampleDraftQuery(BLASTVendor.EMBL_EBI);
@@ -177,4 +197,125 @@ public class DraftQueriesActivityTest extends ActivityInstrumentationTestCase2<D
 		}
 	}
 	
+	public void testWeCanViewTheParametersOfAnEMBLQuery(){
+		
+		BLASTQuery emblQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
+		saveQuery(emblQuery);
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		
+		solo.waitForView(TextView.class);
+		
+		solo.clickLongInList(1);
+		
+		String viewParametersOption = getInstrumentation().getTargetContext().getResources().getString(R.string.view_query_parameters);
+		
+		solo.clickOnText(viewParametersOption);
+		
+		boolean hasATitle = solo.searchText("BLAST Query parameters");
+
+		assertTrue("There should be an appropriate title", hasATitle);
+		
+		boolean programLabelShown = solo.searchText("Program");
+		
+		assertTrue("Should see the program label", programLabelShown);
+		
+		boolean valueOfProgramShown = solo.searchText(emblQuery.getBLASTProgram());
+		
+		assertTrue("Should see the value of the program parameter", valueOfProgramShown);
+		
+		boolean databaseLabelShown = solo.searchText("Program");
+		
+		assertTrue("Should see the database label", databaseLabelShown);
+		
+		boolean valueOfDatabaseShown = solo.searchText(emblQuery.getSearchParameter("database").getValue());
+		
+		assertTrue("Should see the value of the database parameter", valueOfDatabaseShown);
+		
+		boolean expThresholdLabelShown = solo.searchText("Exp. Threshold");
+		
+		assertTrue("Should see the exp threshold label", expThresholdLabelShown);
+		
+		boolean valueOfExpThresholdShown = solo.searchText(emblQuery.getSearchParameter("exp_threshold").getValue());
+		
+		assertTrue("Should see the value of the exp. threshold parameter", valueOfExpThresholdShown);
+		
+		boolean scoreLabelShown = solo.searchText("Score");
+		
+		assertTrue("Should see the score label", scoreLabelShown);
+		
+		boolean valueOfScoreShown = solo.searchText(emblQuery.getSearchParameter("score").getValue());
+		
+		assertTrue("Should see the value of the score parameter", valueOfScoreShown);
+		
+		boolean emailLabelShown = solo.searchText("E-mail");
+		
+		assertTrue("Should see the email label", emailLabelShown);
+		
+		boolean valueOfEmailShown = solo.searchText(emblQuery.getSearchParameter("email").getValue());
+		
+		assertTrue("Should see the value of the e-mail parameter", valueOfEmailShown);
+		
+	}
+
+	public void testThatWeCanViewTheParametersOfAnNCBIQuery(){
+		
+		BLASTQuery ncbiQuery = new BLASTQuery("blastn", BLASTVendor.NCBI);
+		saveQuery(ncbiQuery);
+		
+		solo = new Solo(getInstrumentation(), getActivity());
+		
+		solo.waitForView(TextView.class);
+		
+		solo.clickLongInList(1);
+		
+		String viewParametersOption = getInstrumentation().getTargetContext().getResources().getString(R.string.view_query_parameters);
+		
+		solo.clickOnText(viewParametersOption);
+		
+		boolean hasATitle = solo.searchText("BLAST Query Parameters");
+		
+		assertTrue("Title is the job identifier", hasATitle);
+		
+		boolean programLabelShown = solo.searchText("Program");
+		
+		assertTrue("Should see the program label", programLabelShown);
+		
+		boolean valueOfProgramShown = solo.searchText(ncbiQuery.getBLASTProgram());
+		
+		assertTrue("Should see the value of the program parameter", valueOfProgramShown);
+		
+		boolean databaseLabelShown = solo.searchText("Database");
+		
+		assertTrue("Should see the database label", databaseLabelShown);
+		
+		boolean valueOfDatabaseShown = solo.searchText(ncbiQuery.getSearchParameter("database").getValue());
+		
+		assertTrue("Should see the value of the database parameter", valueOfDatabaseShown);
+		
+		boolean expThresholdLabelShown = solo.searchText("Exp. Threshold");
+		
+		assertTrue("Should see the exp threshold label", expThresholdLabelShown);
+		
+		boolean valueOfExpThresholdShown = solo.searchText(ncbiQuery.getSearchParameter("exp_threshold").getValue());
+		
+		assertTrue("Should see the value of the exp. threshold parameter", valueOfExpThresholdShown);
+		
+		boolean wordSizeLabelShown = solo.searchText("Word Size");
+		
+		assertTrue("Should see the word size label", wordSizeLabelShown);
+		
+		boolean valueOfWordSizeShown = solo.searchText(ncbiQuery.getSearchParameter("word_size").getValue());
+		
+		assertTrue("Should see the value of the word size parameter", valueOfWordSizeShown);
+		
+		boolean matchMismatchScoreLabelShown = solo.searchText("Match/Mis-Match Score");
+		
+		assertTrue("Should see the word size label", matchMismatchScoreLabelShown);
+		
+		boolean valueOfMatchMisMatchScoreShown = solo.searchText(ncbiQuery.getSearchParameter("match_mismatch_score").getValue());
+		
+		assertTrue("Should see the value of the match mis-match score parameter", valueOfMatchMisMatchScoreShown);
+		
+	}
 }
