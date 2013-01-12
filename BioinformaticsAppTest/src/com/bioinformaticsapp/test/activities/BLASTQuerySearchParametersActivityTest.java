f3c9ceb7b66ec054c36caa91e2375cb12eb2d172
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-01-12 06:49:43 +0000

    Test suite that checks we can see the parameters both search engines use in the sequence search. Tests pass at this stage

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/BLASTQuerySearchParametersActivityTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/BLASTQuerySearchParametersActivityTest.java
@@ -5,7 +5,9 @@ import com.bioinformaticsapp.models.BLASTQuery;
 import com.bioinformaticsapp.models.BLASTVendor;
 import com.jayway.android.robotium.solo.Solo;
 
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
 
 public class BLASTQuerySearchParametersActivityTest extends
 		ActivityInstrumentationTestCase2<BLASTQuerySearchParametersActivity> {
@@ -18,7 +20,10 @@ public class BLASTQuerySearchParametersActivityTest extends
 	
 	public void setUp() throws Exception {
 		anyQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		solo = new Solo(getInstrumentation(), getActivity());
+		Intent intent = new Intent();
+		intent.putExtra("query", anyQuery);
+		setActivityIntent(intent);
+		
 	}
 	
 	public void tearDown(){
@@ -26,5 +31,40 @@ public class BLASTQuerySearchParametersActivityTest extends
 		anyQuery = null;
 	}
 	
+	public void testWeCanSeeTheDatabaseParameterDetails(){
+		solo = new Solo(getInstrumentation(), getActivity());
+		solo.waitForView(ListView.class);
+		
+		boolean hasDatabaseLabel = solo.searchText("Database");
+		assertTrue("should display the 'Database' label", hasDatabaseLabel);
+		
+		boolean displaysDatabaseParameter = solo.searchText(anyQuery.getSearchParameter("database").getValue());
+		assertTrue("should display the value of the database parameter", displaysDatabaseParameter);
+		
+	}
+	
+	public void testWeCanSeeTheExpThresholdDetails(){
+		solo = new Solo(getInstrumentation(), getActivity());
+		solo.waitForView(ListView.class);
+		
+		boolean hasExpThresholdLabel = solo.searchText("Exponential Threshold");
+		assertTrue("should display the 'Exponential Threshold' label", hasExpThresholdLabel);
+		
+		boolean displaysExpThresholdParameter = solo.searchText(anyQuery.getSearchParameter("exp_threshold").getValue());
+		assertTrue("should display the value of the database parameter", displaysExpThresholdParameter);
+		
+	}
+	
+	public void testWeCanSeeTheMatchMismatchScoreDetails(){
+		solo = new Solo(getInstrumentation(), getActivity());
+		solo.waitForView(ListView.class);
+		
+		boolean hasMatchMismatchScoreLabel = solo.searchText("Match/Mismatch Score");
+		assertTrue("should display the 'Match/Mismatch Score' label", hasMatchMismatchScoreLabel);
+		
+		boolean displaysMatchMismatchScoreParameter = solo.searchText(anyQuery.getSearchParameter("match_mismatch_score").getValue());
+		assertTrue("should display the value of the match/mismatch score parameter", displaysMatchMismatchScoreParameter);
+		
+	}
 	
 }
