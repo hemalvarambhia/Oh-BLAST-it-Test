978470d13854246b12f67da79994a3b8df4ecdbb
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-01-19 17:59:04 +0000

    Re-factored the test, test object package name was changed

--- BioinformaticsAppTest/src/com/bioinformatics/test/content/BLASTQueryLoaderTest.java
+++ BioinformaticsAppTest/src/com/bioinformatics/test/content/BLASTQueryLoaderTest.java
@@ -2,7 +2,7 @@ package com.bioinformatics.test.content;
 
 import android.test.LoaderTestCase;
 
-import com.bioinformatics.content.BLASTQueryLoader;
+import com.bioinformaticsapp.content.BLASTQueryLoader;
 import com.bioinformaticsapp.models.BLASTQuery;
 import com.bioinformaticsapp.models.BLASTQuery.Status;
 import com.bioinformaticsapp.models.BLASTVendor;
@@ -11,30 +11,34 @@ import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
 public class BLASTQueryLoaderTest extends LoaderTestCase {
 
 	private BLASTQueryLoader mBLASTQueryloader;
-
+	private OhBLASTItTestHelper helper;
 	public void setUp() throws Exception {
 		super.setUp();
+		helper = new OhBLASTItTestHelper(getContext());
+		helper.cleanDatabase();	
+	}
+	
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testWeCanLoadQueriesByStatus(){
 		BLASTQuery draftQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
 		BLASTQuery finished = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
 		finished.setStatus(Status.FINISHED);
-		OhBLASTItTestHelper helper = new OhBLASTItTestHelper(getContext());
-		helper.cleanDatabase();
 		helper.save(draftQuery);
 		helper.save(finished);
 		
 		mBLASTQueryloader = new BLASTQueryLoader(getContext(), Status.DRAFT);
-	}
-	
-	public void tearDown() throws Exception {
-		mBLASTQueryloader.stopLoading();
-		mBLASTQueryloader = null;
-		super.tearDown();
-	}
-	
-	public void testWeCanRetrieveDraftQueries(){
+		
 		BLASTQuery[] draftQueries = getLoaderResultSynchronously(mBLASTQueryloader);
 		
 		assertEquals(1, draftQueries.length);
+		
+		for(BLASTQuery query: draftQueries){
+			assertEquals("Should be able to load queries by status", Status.DRAFT, query.getStatus());
+		}
+		
 	}
 	
 }
