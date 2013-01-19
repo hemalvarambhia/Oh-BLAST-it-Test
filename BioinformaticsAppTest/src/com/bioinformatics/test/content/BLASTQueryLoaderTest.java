18726e8c06277eba5c5a467683468997d6af59bc
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-01-19 10:53:52 +0000

    Tests for the development of the query loader

---
+++ BioinformaticsAppTest/src/com/bioinformatics/test/content/BLASTQueryLoaderTest.java
@@ -0,0 +1,42 @@
+package com.bioinformatics.test.content;
+
+import java.util.List;
+
+import com.bioinformatics.content.BLASTQueryLoader;
+import com.bioinformaticsapp.models.BLASTQuery;
+import com.bioinformaticsapp.models.BLASTQuery.Status;
+import com.bioinformaticsapp.models.BLASTVendor;
+import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
+
+import android.test.LoaderTestCase;
+
+public class BLASTQueryLoaderTest extends LoaderTestCase {
+
+	private BLASTQueryLoader mBLASTQueryloader;
+
+	public void setUp() throws Exception {
+		super.setUp();
+		BLASTQuery draftQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
+		BLASTQuery finished = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
+		finished.setStatus(Status.FINISHED);
+		OhBLASTItTestHelper helper = new OhBLASTItTestHelper(getContext());
+		helper.cleanDatabase();
+		helper.save(draftQuery);
+		helper.save(finished);
+		
+		mBLASTQueryloader = new BLASTQueryLoader(getContext(), Status.DRAFT);
+	}
+	
+	public void tearDown() throws Exception {
+		mBLASTQueryloader.stopLoading();
+		mBLASTQueryloader = null;
+		super.tearDown();
+	}
+	
+	public void testWeCanRetrieveDraftQueries(){
+		List<BLASTQuery> draftQueries = getLoaderResultSynchronously(mBLASTQueryloader);
+		
+		assertEquals(1, draftQueries.size());
+	}
+	
+}
