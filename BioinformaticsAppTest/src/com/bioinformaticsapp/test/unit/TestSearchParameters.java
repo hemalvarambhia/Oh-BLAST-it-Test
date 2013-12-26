456ed210fbe6658fa9bd62cdadae67eac8424aa0
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-12-26 23:16:09 +0000

    Made tests more readable, expressive

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/unit/TestSearchParameters.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/unit/TestSearchParameters.java
@@ -1,12 +1,10 @@
 package com.bioinformaticsapp.test.unit;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.hamcrest.MatcherAssert;
-
-import static org.hamcrest.core.Is.*;
-import static org.hamcrest.core.IsEqual.*;
 
 import junit.framework.TestCase;
 
@@ -17,25 +15,33 @@ public class TestSearchParameters extends TestCase {
 
 	public void testWeCanCreateDefaultOptionalParametersForEMBLEBI(){	
 		List<SearchParameter> searchParameters = SearchParameter.createDefaultParametersFor(BLASTVendor.EMBL_EBI);
+		
+		assertThat(searchParameters, is(emblDefaults()));
+	}
+	
+	public void testWeCanCreateDefaultOptionalParametersForNCBI(){
+		List<SearchParameter> searchParameters = SearchParameter.createDefaultParametersFor(BLASTVendor.NCBI);
+		
+		assertThat(searchParameters, is(ncbiDefaults()));
+	}
+	
+	private List<SearchParameter> ncbiDefaults(){
+		List<SearchParameter> expectedOptionalParameters = new ArrayList<SearchParameter>();
+		expectedOptionalParameters.add(new SearchParameter("database", "nr"));
+		expectedOptionalParameters.add(new SearchParameter("word_size", "28"));
+		expectedOptionalParameters.add(new SearchParameter("exp_threshold", "10"));
+		expectedOptionalParameters.add(new SearchParameter("match_mismatch_score", "1,-2"));
+		
+		return expectedOptionalParameters;
+	}
+	
+	private List<SearchParameter> emblDefaults(){
 		List<SearchParameter> expectedOptionalParameters = new ArrayList<SearchParameter>();
 		expectedOptionalParameters.add(new SearchParameter("database", "em_rel_fun"));
 		expectedOptionalParameters.add(new SearchParameter("exp_threshold", "10"));
 		expectedOptionalParameters.add(new SearchParameter("score", "50"));
 		expectedOptionalParameters.add(new SearchParameter("match_mismatch_score", "1,-2"));
 		expectedOptionalParameters.add(new SearchParameter("email", ""));
-		
-		MatcherAssert.assertThat(searchParameters, is(equalTo(expectedOptionalParameters)));
-	}
-	
-	public void testWeCanCreateDefaultOptionalParametersForNCBI(){
-		List<SearchParameter> searchParameters = SearchParameter.createDefaultParametersFor(BLASTVendor.NCBI);
-		List<SearchParameter> expectedOptionalParameters = new ArrayList<SearchParameter>();
-		
-		expectedOptionalParameters.add(new SearchParameter("database", "nr"));
-		expectedOptionalParameters.add(new SearchParameter("word_size", "28"));
-		expectedOptionalParameters.add(new SearchParameter("exp_threshold", "10"));
-		expectedOptionalParameters.add(new SearchParameter("match_mismatch_score", "1,-2"));
-		
-		assertEquals(expectedOptionalParameters, searchParameters);
+		return expectedOptionalParameters;
 	}
 }
