118563cf037b1ad07f6f5f66ab13f1ba8480d004
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-12-28 08:54:59 +0000

    Using Hamcrest to make the tests more readable

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/helpers/TestStatusTranslator.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/helpers/TestStatusTranslator.java
@@ -3,8 +3,11 @@ package com.bioinformaticsapp.test.helpers;
 import junit.framework.TestCase;
 
 import com.bioinformaticsapp.helpers.StatusTranslator;
-import com.bioinformaticsapp.models.BLASTQuery.Status;
+import static com.bioinformaticsapp.models.BLASTQuery.Status;
 import com.bioinformaticsapp.web.SearchStatus;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.*;
 
 public class TestStatusTranslator extends TestCase {
 
@@ -14,38 +17,34 @@ public class TestStatusTranslator extends TestCase {
 		translator = new StatusTranslator();
 	}
 	
-	public void tearDown(){
-		translator = null;
-	}
-	
 	public void testThatSearchStatusRUNNINGYieldsSENT(){
 		Status status = translator.translate(SearchStatus.RUNNING);
 		
-		assertEquals("RUNNING means it was SUBMITTED", Status.SUBMITTED, status);
+		assertThat("RUNNING means it was SUBMITTED", status, is(Status.SUBMITTED));
 	}
 	
 	public void testThatSearchStatusFINISHEDYieldsFINISHED(){
 		Status status = translator.translate(SearchStatus.FINISHED);
 		
-		assertEquals("FINISHED means FINISHED", Status.FINISHED, status);
+		assertThat("FINISHED means FINISHED", status, is(Status.FINISHED));
 	}
 	
 	public void testThatSearchStatusERRORYieldsERROR(){
 		Status status = translator.translate(SearchStatus.ERROR);
 		
-		assertEquals("ERROR means ERROR", Status.ERROR, status);
+		assertThat("ERROR means ERROR", status, is(Status.ERROR));
 	}
 	
 	public void testThatSearchStatusNOT_FOUNDYieldsNOT_FOUND(){
 		Status status = translator.translate(SearchStatus.NOT_FOUND);
 		
-		assertEquals("NOT_FOUND means NOT_FOUND", Status.NOT_FOUND, status);
+		assertThat("NOT_FOUND means NOT_FOUND", status, is(Status.NOT_FOUND));
 	}
 	
 	public void testThatUNSUREYieldsUNSURE(){
 		Status status = translator.translate(SearchStatus.UNSURE);
 		
-		assertEquals("UNSURE means UNSURE", Status.UNSURE, status);
+		assertThat("UNSURE means UNSURE", status, is(Status.UNSURE));
 	}
 	
 }
