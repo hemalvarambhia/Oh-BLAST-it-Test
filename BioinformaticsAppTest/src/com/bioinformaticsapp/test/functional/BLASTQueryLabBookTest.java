fb2effa09fa5bc6adc115f60bed3354a536b3ef6
Hemal Varambhia <h.n.varambhia@gmail.com>
2013-12-30 12:21:37 +0000

    Got the test for saving a query working.

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryLabBookTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryLabBookTest.java
@@ -1,14 +1,14 @@
 package com.bioinformaticsapp.test.functional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.Is.*;
-import static org.hamcrest.core.IsNull.*;
+import static org.hamcrest.core.IsNull.notNullValue;
 import android.test.InstrumentationTestCase;
 
 import com.bioinformaticsapp.data.BLASTQueryLabBook;
 import com.bioinformaticsapp.models.BLASTQuery;
-import com.bioinformaticsapp.models.BLASTVendor;
+import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
 
 public class BLASTQueryLabBookTest extends InstrumentationTestCase {
 
@@ -17,17 +17,22 @@ public class BLASTQueryLabBookTest extends InstrumentationTestCase {
 	
 	public void setUp() throws Exception {
 		super.setUp();
-		aQuery = new BLASTQuery("blastn", BLASTVendor.NCBI);
+		OhBLASTItTestHelper helper = new OhBLASTItTestHelper(getInstrumentation().getTargetContext());
+		helper.cleanDatabase();
+		aQuery = BLASTQuery.ncbiBLASTQuery("blastn");
 		labBook = new BLASTQueryLabBook(getInstrumentation().getTargetContext());
 	}
 	
 	public void testWeCanSaveTheBLASTQueryToTheLabBook(){
 		BLASTQuery query = labBook.save(aQuery);
 		
-		assertThat("Should be able to save a BLASTQuery to the lab book",
+		assertThat("Saved BLAST query must have a primary key",
 				query.getPrimaryKey(), is(notNullValue()));
+		
+		BLASTQuery expected = (BLASTQuery)aQuery.clone();
+		expected.setPrimaryKeyId(query.getPrimaryKey());
 		assertThat("Should be able to save a BLASTQuery to the lab book",
-				query, equalTo(aQuery));
+				query, equalTo(expected));
 	}
 	
 	
