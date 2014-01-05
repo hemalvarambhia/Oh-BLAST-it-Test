5387c6d3f03071964a2f6608ee9db85bb0a8a06e
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-01-05 14:14:24 +0000

    More test clean ups and expressive assertions using Hamcrest

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryCRUDTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryCRUDTest.java
@@ -2,36 +2,30 @@ package com.bioinformaticsapp.test.functional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.*;
-import static org.hamcrest.core.IsNull.*;
+import static org.hamcrest.core.IsNull.nullValue;
 
 import java.util.List;
 
 import android.test.InstrumentationTestCase;
 
 import com.bioinformaticsapp.data.BLASTQueryController;
-import com.bioinformaticsapp.data.SearchParameterController;
 import com.bioinformaticsapp.models.BLASTQuery;
-import com.bioinformaticsapp.models.BLASTQuery.Status;
+import static com.bioinformaticsapp.models.BLASTQuery.Status;
 import com.bioinformaticsapp.models.BLASTVendor;
-import com.bioinformaticsapp.models.SearchParameter;
 import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
 
 public class BLASTQueryCRUDTest extends InstrumentationTestCase {
 
-	private static final String TAG = "BLASTQueryCRUDTest";
 	private BLASTQueryController controller;
-	private SearchParameterController searchParameterController;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
 		OhBLASTItTestHelper helper = new OhBLASTItTestHelper(getInstrumentation().getTargetContext());
 		helper.cleanDatabase();
 		controller = new BLASTQueryController(getInstrumentation().getTargetContext());
-		searchParameterController = new SearchParameterController(getInstrumentation().getTargetContext());
 	}
 	
 	protected void tearDown() throws Exception {
-		searchParameterController.close();
 		controller.close();
 		super.tearDown();	
 	}
@@ -43,7 +37,6 @@ public class BLASTQueryCRUDTest extends InstrumentationTestCase {
 
 		return queryInDatabase;				
 	}
-
 	
 	public void testWeCanSaveABLASTQueryToDatabase(){
 		BLASTQuery draftQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
@@ -53,63 +46,44 @@ public class BLASTQueryCRUDTest extends InstrumentationTestCase {
 		assertThat("Should be able to save a BLASTQuery", primaryKeyId > 0);
 	}
 	
-	
-	
 	public void testWeCanRetrieveABLASTQueryByPrimaryKey(){
 		BLASTQuery queryInDatabase = queryAsInsertedInDatebase();
 		
 		BLASTQuery retrievedFromDatabase = controller.findBLASTQueryById(queryInDatabase.getPrimaryKey());
+		
 		assertThat("Should be able to retrive a query by its ID", retrievedFromDatabase, is(queryInDatabase));
-	
 	}
 	
 	public void testWeCanRetrieveABLASTQueryWithDRAFTStatus(){
-		//Default status is DRAFT so no need to explicitly set status to DRAFT
 		BLASTQuery draft = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		
-		long primaryKey = controller.save(draft);
+		controller.save(draft);
 		
 		List<BLASTQuery> draftQueries = controller.findBLASTQueriesByStatus(Status.DRAFT);
+		
 		BLASTQuery firstDraftQuery = draftQueries.get(0);
-		
-		assertEquals("Expected primary key", primaryKey, firstDraftQuery.getPrimaryKey().longValue());
-		
+		assertThat("Expected a draft status",  firstDraftQuery.getStatus(), is(Status.DRAFT));
 	}
 	
 	public void testWeCanRetrieveABLASTQueryByStatus(){
-		//Default status is DRAFT so no need to explicitly set status to DRAFT
+		//Default status is DRAFT
 		BLASTQuery draft = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		
 		controller.save(draft);
-		
-		BLASTQuery finished = new BLASTQuery("blastn", BLASTVendor.NCBI);
-		finished.setStatus(Status.FINISHED);
-		controller.save(finished);
-		
-		BLASTQuery submitted = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
+		BLASTQuery submitted = new BLASTQuery("blastn", BLASTVendor.NCBI);
 		submitted.setStatus(Status.SUBMITTED);
-		long expectedPrimaryKey = controller.save(submitted);
+		controller.save(submitted);
 		
 		List<BLASTQuery> submittedQueries = controller.findBLASTQueriesByStatus(Status.SUBMITTED);
-		BLASTQuery firstSubmittedQuery = submittedQueries.get(0);
-		assertTrue(submittedQueries.size() == 1);
 		
-		assertEquals(expectedPrimaryKey, firstSubmittedQuery.getPrimaryKey().longValue());
-		
+		assertThat(submittedQueries.size(), is(1));
+		BLASTQuery submittedQuery = submittedQueries.get(0);
+		assertThat("Should be able to retrieve a query by its status", submittedQuery.getStatus(), is(BLASTQuery.Status.SUBMITTED));
 	}
 	
 	private void insertBLASTQueryWithStatus(BLASTQuery.Status status){
 		BLASTQuery sampleQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
 		sampleQuery.setStatus(status);
 		
-		long blastQueryId = controller.save(sampleQuery);
-		for(SearchParameter parameter: sampleQuery.getAllParameters()){
-			parameter.setBlastQueryId(blastQueryId);			
-			searchParameterController.save(parameter);
-		}
-		
-		//controller is closed in the tearDown message so no need to do it here
-		
+		controller.save(sampleQuery);
 	}
 	
 	public void testWeCanRetrieveSubmittedBLASTQueries(){
@@ -118,26 +92,17 @@ public class BLASTQueryCRUDTest extends InstrumentationTestCase {
 		
 		List<BLASTQuery> runningAndSubmittedQueries = controller.getSubmittedBLASTQueries();
 		
-		assertEquals(1, runningAndSubmittedQueries.size());
-		
+		assertThat(runningAndSubmittedQueries.size(), is(1));	
 	}
 	
 	public void testWeCanUpdateAQuery(){
 		BLASTQuery draftQuery = queryAsInsertedInDatebase();
+		draftQuery.setSequence("CTAGTTTT");
+		int noUpdated = controller.update(draftQuery);
+		
+		assertThat("Should be able to update a BLASTQuery", noUpdated, is(1));
 		BLASTQuery retrieved = controller.findBLASTQueryById(draftQuery.getPrimaryKey());
-		List<SearchParameter> parameters = searchParameterController.getParametersForQuery(retrieved.getPrimaryKey());
-		retrieved.updateAllParameters(parameters);
-		retrieved.setSearchParameter("match_mismatch_score", "1, -1");
-		retrieved.setSearchParameter("exp_threshold", "0.1");
-		
-		int noUpdated = controller.update(retrieved.getPrimaryKey(), retrieved);
-		
-		for(SearchParameter parameter: retrieved.getAllParameters()){
-			noUpdated += searchParameterController.update(parameter.getPrimaryKey(), parameter);
-		}
-		
-		assertEquals(retrieved.getAllParameters().size() + 1, noUpdated);
-		
+		assertThat(retrieved, is(draftQuery));
 	}
 	
 	public void testWeCanDeleteAQuery(){
