4abe2b8695e969ea3d91f46535fa554d17dd1c85
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-02-15 19:14:19 +0000

    More clean up

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryPollerTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryPollerTest.java
@@ -6,7 +6,6 @@ import java.util.concurrent.ExecutionException;
 import android.content.Context;
 import android.test.InstrumentationTestCase;
 
-import com.bioinformaticsapp.data.BLASTQueryLabBook;
 import com.bioinformaticsapp.models.BLASTQuery;
 import com.bioinformaticsapp.models.BLASTVendor;
 import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
@@ -28,77 +27,49 @@ public class BLASTQueryPollerTest extends InstrumentationTestCase {
 	}
 	
 	public void testWeCanPollASUBMITTEDEMBLQuery() throws InterruptedException, ExecutionException{
-		createPendingEMBLBLASTQuery();
+		emblQuery = createPendingEMBLBLASTQuery();
 		waitUntilSent(emblQuery);
 		
 		poller.execute(emblQuery);
 		
-		try {
-			poller.get();
-		} catch (InterruptedException e) {
-			fail("Task was interrupted unexpectedly");
-		} catch (ExecutionException e) {
-			fail("There was a problem in poller the query");
-		}
+		waitFor(poller);
+		
 		BLASTQuery.Status[] possibilities = new BLASTQuery.Status[] { BLASTQuery.Status.SUBMITTED, BLASTQuery.Status.FINISHED };
 		boolean pollingYieldsValidStatus = Arrays.binarySearch(possibilities, emblQuery.getStatus()) > -1;
 		assertTrue(pollingYieldsValidStatus);
-		
 	}
 	
 	public void testWeCanPollASUBMITTEDNCBIQuery() throws InterruptedException, ExecutionException{
-		createPendingNCBIBLASTQuery();
+		ncbiQuery = createPendingNCBIBLASTQuery();
 		waitUntilSent(ncbiQuery);
 		
 		poller.execute(ncbiQuery);
 		
-		try {
-			poller.get();
-		} catch (InterruptedException e) {
-			fail("Task was interrupted unexpectedly");
-		} catch (ExecutionException e) {
-			fail("There was a problem in polling the query");
-		}
+		waitFor(poller);
 		BLASTQuery.Status[] possibilities = new BLASTQuery.Status[] { BLASTQuery.Status.SUBMITTED, BLASTQuery.Status.FINISHED };
 		boolean pollingYieldsValidStatus = Arrays.binarySearch(possibilities, ncbiQuery.getStatus()) > -1;
 		assertTrue(pollingYieldsValidStatus);
-		
 	}
 	
 	public void testWeCanPollMoreThanOneBLASTQuery() throws InterruptedException, ExecutionException{
-		
 		BLASTQuery[] queries = new BLASTQuery[2];
-		
 		for(int i = 0; i < 2; i++){
-			queries[i] = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-			queries[i].setSearchParameter("email", "h.n.varambhia@gmail.com");
-			queries[i].setSequence("CCTTTATCTAATCTTTGGAGCATGAGCTGG");
-			queries[i].setStatus(BLASTQuery.Status.PENDING);
-		}
-		
+			queries[i] = createPendingEMBLBLASTQuery();
+		}	
 		waitUntilSent(queries);
-		
-		
+	
 		poller.execute(queries);
 		
-		try {
-			poller.get();
-		} catch (InterruptedException e) {
-			fail("Task was interrupted unexpectedly");
-		} catch (ExecutionException e) {
-			fail("There was a problem in poller the query");
-		}
+		waitFor(poller);
 		BLASTQuery.Status[] possibilities = new BLASTQuery.Status[] { BLASTQuery.Status.SUBMITTED, BLASTQuery.Status.FINISHED };
-		
 		for(BLASTQuery query: queries){
 			boolean pollingYieldsValidStatus = Arrays.binarySearch(possibilities, query.getStatus()) > -1;
 			assertTrue("query does not have a status of SUBMITTED or FINISHED; it has a status of "+query.getStatus(), pollingYieldsValidStatus);
 		} 
-		
 	}
 	
 	public void testWeDoNotPollWhenThereIsNoWebConnection() throws InterruptedException, ExecutionException{
-		createPendingEMBLBLASTQuery();
+		emblQuery = createPendingEMBLBLASTQuery();
 		waitUntilSent(emblQuery);
 		
 		final BLASTQueryPoller poller = new BLASTQueryPoller(context){
@@ -107,9 +78,19 @@ public class BLASTQueryPollerTest extends InstrumentationTestCase {
 			}
 		};
 		
-		
 		poller.execute(emblQuery);
 		
+		waitFor(poller);
+		assertEquals("Expected query's status to be unchanged from when it was submitted", BLASTQuery.Status.SUBMITTED, emblQuery.getStatus());
+	}
+	
+	private void waitUntilSent(BLASTQuery... queries) throws InterruptedException, ExecutionException{
+		BLASTQuerySender sender = new BLASTQuerySender(context);
+		sender.execute(queries);
+		sender.get();
+	}
+	
+	private void waitFor(BLASTQueryPoller poller){
 		try {
 			poller.get();
 		} catch (InterruptedException e) {
@@ -117,34 +98,20 @@ public class BLASTQueryPollerTest extends InstrumentationTestCase {
 		} catch (ExecutionException e) {
 			fail("Task errored out unexpectedly");
 		}
-		
-		assertEquals("Expected query's status to be unchanged from when it was submitted", BLASTQuery.Status.SUBMITTED, emblQuery.getStatus());
-		
 	}
 	
-	private void createPendingEMBLBLASTQuery(){
-		emblQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
+	private BLASTQuery createPendingEMBLBLASTQuery(){
+		BLASTQuery emblQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
 		emblQuery.setSearchParameter("email", "h.n.varambhia@gmail.com");
 		emblQuery.setSequence("CCTTTATCTAATCTTTGGAGCATGAGCTGG");
 		emblQuery.setStatus(BLASTQuery.Status.PENDING);
-		save(emblQuery);
+		return emblQuery;
 	}
 	
-	private void createPendingNCBIBLASTQuery(){
-		ncbiQuery = new BLASTQuery("blastn", BLASTVendor.NCBI);
+	private BLASTQuery createPendingNCBIBLASTQuery(){
+		BLASTQuery ncbiQuery = new BLASTQuery("blastn", BLASTVendor.NCBI);
 		ncbiQuery.setSequence("CCTTTATCTAATCTTTGGAGCATGAGCTGG");
 		ncbiQuery.setStatus(BLASTQuery.Status.PENDING);
-		save(ncbiQuery);
-	}
-
-	private void save(BLASTQuery query){
-		BLASTQueryLabBook labBook = new BLASTQueryLabBook(context);
-		labBook.save(query);
-	}
-	
-	private void waitUntilSent(BLASTQuery... queries) throws InterruptedException, ExecutionException{
-		BLASTQuerySender sender = new BLASTQuerySender(context);
-		sender.execute(queries);
-		sender.get();	
+		return ncbiQuery;
 	}
 }
