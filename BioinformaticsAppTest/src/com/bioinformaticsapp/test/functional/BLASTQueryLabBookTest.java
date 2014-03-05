84fc9c15471d497aa6d69d7c02bd695ba0899db8
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-03-05 20:24:02 +0000

    Test method for pending queries to be sent to a BLAST querying provider added

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryLabBookTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQueryLabBookTest.java
@@ -11,6 +11,8 @@ import android.test.InstrumentationTestCase;
 
 import com.bioinformaticsapp.data.BLASTQueryLabBook;
 import com.bioinformaticsapp.models.BLASTQuery;
+import com.bioinformaticsapp.models.BLASTQuery.Status;
+import com.bioinformaticsapp.models.BLASTVendor;
 import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
 
 public class BLASTQueryLabBookTest extends InstrumentationTestCase {
@@ -22,11 +24,11 @@ public class BLASTQueryLabBookTest extends InstrumentationTestCase {
 		super.setUp();
 		OhBLASTItTestHelper helper = new OhBLASTItTestHelper(getInstrumentation().getTargetContext());
 		helper.cleanDatabase();
-		aQuery = BLASTQuery.ncbiBLASTQuery("blastn");
 		labBook = new BLASTQueryLabBook(getInstrumentation().getTargetContext());
 	}
 	
 	public void testWeCanSaveTheBLASTQueryToTheLabBook(){
+		aQuery = aBLASTQuery();
 		BLASTQuery query = labBook.save(aQuery);
 		
 		assertThat("Saved BLAST query must have a primary key",
@@ -34,6 +36,7 @@ public class BLASTQueryLabBookTest extends InstrumentationTestCase {
 	}
 	
 	public void testWeCanRetrieveAQueryByIdentifier(){
+		aQuery = aBLASTQuery();
 		BLASTQuery query = labBook.save(aQuery);
 		
 		BLASTQuery fromStorage= labBook.findQueryById(query.getPrimaryKey());
@@ -42,6 +45,7 @@ public class BLASTQueryLabBookTest extends InstrumentationTestCase {
 	}
 	
 	public void testWeCanEditAnExistingQuery(){
+		aQuery = aBLASTQuery();
 		BLASTQuery query = labBook.save(aQuery);
 		query.setSearchParameter("email", "h.n.varambhia@gmail.com");
 		long primaryKey = query.getPrimaryKey();
@@ -53,15 +57,54 @@ public class BLASTQueryLabBookTest extends InstrumentationTestCase {
 	}
 	
 	public void testWeCanRetrieveBLASTQueriesWithAStatus(){
+		aQuery = aBLASTQueryWithStatus(Status.DRAFT);
 		aQuery = labBook.save(aQuery);
-		BLASTQuery query = BLASTQuery.emblBLASTQuery("blastn");
-		query.setStatus(BLASTQuery.Status.FINISHED);
-		labBook.save(query);
+		BLASTQuery queryWithDifferentStatus = aBLASTQueryWithStatus(Status.FINISHED);
+		labBook.save(queryWithDifferentStatus);
 		
 		List<BLASTQuery> drafts = labBook.findBLASTQueriesByStatus(BLASTQuery.Status.DRAFT);
 		
 		assertThat("Should be able to find BLAST queries by their status", drafts.contains(aQuery));
-		assertThat("Should not contain queries with a status different to the one required", !drafts.contains(query));
+		assertThat("Should not contain queries with a status different to the one required", !drafts.contains(queryWithDifferentStatus));
+	}
+	
+	public void testWeCanRetrievesNoPendingBLASTQueriesIfThereAreNone(){
+		List<BLASTQuery> pendingQueries = labBook.findPendingBLASTQueriesFor(BLASTVendor.NCBI);
+		
+		assertThat("list should not contain any pending blast queries for a supplier if there are none", pendingQueries.isEmpty());
+	}
+	
+	public void testWeCanRetrievesPendingBLASTQueriesToBeSentToSupplier(){
+		BLASTQuery pending = aBLASTQueryWithStatus(Status.PENDING, BLASTVendor.NCBI);
+		pending = labBook.save(pending);
+		
+		List<BLASTQuery> pendingQueries = labBook.findPendingBLASTQueriesFor(BLASTVendor.NCBI);
+		
+		assertThat("list should contain pending blast queries for a supplier", pendingQueries.contains(pending));
+	}
+	
+	private BLASTQuery aBLASTQueryWithStatus(Status status){
+		BLASTQuery blastQuery = aBLASTQuery();
+		blastQuery.setStatus(status);
+		return blastQuery;
+	}
+	
+	private BLASTQuery aBLASTQueryWithStatus(Status status, int vendor){
+		BLASTQuery blastQuery = aBLASTQuery();
+		switch(vendor){
+		case BLASTVendor.EMBL_EBI:
+			blastQuery = BLASTQuery.emblBLASTQuery("blastn");
+			break;
+		case BLASTVendor.NCBI:
+			blastQuery = BLASTQuery.ncbiBLASTQuery("blastn");
+			break;
+		}
+		blastQuery.setStatus(status);
+		return blastQuery;
+	}
+
+	private BLASTQuery aBLASTQuery(){
+		return BLASTQuery.emblBLASTQuery("blastn");
 	}
 	
 }
