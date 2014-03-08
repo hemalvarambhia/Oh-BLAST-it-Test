83ad52eac745d967b556b3aeeef6fdccf0f6d89b
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-03-08 18:57:27 +0000

    Removed duplication from sending the queries.

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQuerySenderTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/BLASTQuerySenderTest.java
@@ -53,14 +53,52 @@ public class BLASTQuerySenderTest extends InstrumentationTestCase {
 	}
 	
 	public void testBLASTQueryHasAJobIdentifierWhenSentSuccessfully(){
+		send(new BLASTQuery[]{query});
 		
+		assertNotNull("was expecting a job identifier", query.getJobIdentifier());
+		assertTrue( !query.getJobIdentifier().isEmpty() );		
+	}
+	
+	public void testWeCanSendAnNCBIQuery(){
+		send(new BLASTQuery[]{query});					
+		
+		assertNotNull("Query was not assigned a job identifier by the service", query.getJobIdentifier());
+		assertFalse("Job identifier was found to be an empty string", query.getJobIdentifier().isEmpty());
+		assertEquals("Query status was not updated to SUBMITTED", Status.SUBMITTED, query.getStatus());
+	}
+	
+	public void testWeCanSendAnEBI_EMBLQuery(){
+		send(new BLASTQuery[]{emblQuery});					
+		
+		assertNotNull("Query was not assigned a job identifier by the service", emblQuery.getJobIdentifier());
+		assertFalse("Job identifier was found to be an empty string", emblQuery.getJobIdentifier().isEmpty());
+		Log.i(TAG, emblQuery.getJobIdentifier());
+		assertEquals("Query status was not updated to SUBMITTED", Status.SUBMITTED, emblQuery.getStatus());
+		
+	}
+	
+	public void testWeCanSendMoreThanOneQuery(){
+		
+		final BLASTQuery[] pendingBlastQueries = manyValidPendingBLASTQueries();
+		
+		send(pendingBlastQueries);					
+		
+		for(BLASTQuery query : pendingBlastQueries){
+			assertNotNull("Query was not assigned a job identifier by the service", query.getJobIdentifier());
+			assertFalse("Job identifier was found to be an empty string", query.getJobIdentifier().isEmpty());
+			Log.i(TAG, query.getJobIdentifier());
+			assertEquals("Query status was not updated to SUBMITTED", Status.SUBMITTED, query.getStatus());
+		}		
+	}
+	
+	private void send(final BLASTQuery[] queries){
 		final BLASTQuerySender sender = new BLASTQuerySender(context);
 		
 		try {
 			runTestOnUiThread(new Runnable() {
 				
 				public void run() {
-					sender.execute(new BLASTQuery[]{query});
+					sender.execute(queries);
 					
 				}
 			});
@@ -75,140 +113,6 @@ public class BLASTQuerySenderTest extends InstrumentationTestCase {
 		} catch (ExecutionException e) {
 			fail();
 		}
-		
-		assertNotNull("was expecting a job identifier", query.getJobIdentifier());
-		assertTrue( !query.getJobIdentifier().isEmpty() );		
-	}
-	
-	public void testQueriesNotSentWhenThereIsNoWebConnection(){
-		
-		final BLASTQuerySender sender = new BLASTQuerySender(context){
-			protected boolean connectedToWeb(){
-				return false;
-			}
-		};
-		
-		try {
-			runTestOnUiThread(new Runnable() {
-				
-				public void run() {
-					sender.execute(new BLASTQuery[]{query});					
-				}
-			});
-		} catch (Throwable e) {
-			fail();
-		}
-		
-		Integer numberOfQueries = null;
-		try {
-			numberOfQueries = sender.get();
-		} catch (InterruptedException e) {
-			fail("Execution of the thread was interrupted");
-		} catch (ExecutionException e) {
-			fail();
-		}
-		
-		assertEquals("Query should be pending", BLASTQuery.Status.PENDING, query.getStatus());
-		assertEquals("Query should not be sent", 0, numberOfQueries.intValue());
-		assertNull("Query status should be PENDING when it is valid but there is no web connection", query.getJobIdentifier());
-
-	}
-	
-	public void testWeCanSendAnNCBIQuery(){
-		
-		final BLASTQuerySender sender = new BLASTQuerySender(context);
-		
-		try {
-			runTestOnUiThread(new Runnable() {
-				
-				public void run() {
-					sender.execute(new BLASTQuery[]{query});					
-				}
-			});
-		} catch (Throwable e) {
-			fail();
-		}
-		
-		Integer numberSent = null;
-		try {
-			numberSent = sender.get();
-		} catch (InterruptedException e) {
-			fail("Execution of the thread was interrupted");
-		} catch (ExecutionException e) {
-			fail();
-		}
-		Log.i(TAG, query.getJobIdentifier());
-		assertNotNull("Query was not assigned a job identifier by the service", query.getJobIdentifier());
-		assertFalse("Job identifier was found to be an empty string", query.getJobIdentifier().isEmpty());
-		assertEquals("Query status was not updated to SUBMITTED", Status.SUBMITTED, query.getStatus());
-		assertEquals("Query should have been sent", 1, numberSent.intValue());
-	}
-	
-	public void testWeCanSendAnEBI_EMBLQuery(){
-		
-		final BLASTQuerySender sender = new BLASTQuerySender(context);
-		
-		try {
-			runTestOnUiThread(new Runnable() {
-				
-				public void run() {
-					sender.execute(new BLASTQuery[]{emblQuery});					
-				}
-			});
-		} catch (Throwable e) {
-			fail();
-		}
-		
-		try {
-			sender.get();
-		} catch (InterruptedException e) {
-			fail("Execution of the thread was interrupted");
-		} catch (ExecutionException e) {
-			fail(e.getMessage());
-		}
-		
-		assertNotNull("Query was not assigned a job identifier by the service", emblQuery.getJobIdentifier());
-		assertFalse("Job identifier was found to be an empty string", emblQuery.getJobIdentifier().isEmpty());
-		Log.i(TAG, emblQuery.getJobIdentifier());
-		assertEquals("Query status was not updated to SUBMITTED", Status.SUBMITTED, emblQuery.getStatus());
-		
-	}
-	
-	public void testWeCanSendMoreThanOneQuery(){
-		
-		final BLASTQuery[] pendingBlastQueries = manyValidPendingBLASTQueries();
-		
-		
-		final BLASTQuerySender sender = new BLASTQuerySender(context);
-		
-		try {
-			runTestOnUiThread(new Runnable() {
-				
-				public void run() {
-					sender.execute(pendingBlastQueries);					
-				}
-			});
-		} catch (Throwable e) {
-			fail();
-		}
-		
-		Integer numberOfQueriesSent = null;
-		try {
-			numberOfQueriesSent = sender.get();
-		} catch (InterruptedException e) {
-			fail("Execution of the thread was interrupted");
-		} catch (ExecutionException e) {
-			fail(e.getMessage());
-		}
-		
-		assertEquals("All valid queries should be sent", pendingBlastQueries.length, numberOfQueriesSent.intValue());
-		
-		for(BLASTQuery query : pendingBlastQueries){
-			assertNotNull("Query was not assigned a job identifier by the service", query.getJobIdentifier());
-			assertFalse("Job identifier was found to be an empty string", query.getJobIdentifier().isEmpty());
-			Log.i(TAG, query.getJobIdentifier());
-			assertEquals("Query status was not updated to SUBMITTED", Status.SUBMITTED, query.getStatus());
-		}		
 	}
 	
 	private BLASTQuery[] manyValidPendingBLASTQueries(){
