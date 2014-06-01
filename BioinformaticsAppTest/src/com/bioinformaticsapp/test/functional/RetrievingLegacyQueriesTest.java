edfcca6666d1d34598846ef669d1248c3000f148
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-06-01 15:41:26 +0100

    Clean up with Hamcrest

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/RetrievingLegacyQueriesTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/functional/RetrievingLegacyQueriesTest.java
@@ -3,12 +3,13 @@ package com.bioinformaticsapp.test.functional;
 import java.util.List;
 
 import android.test.InstrumentationTestCase;
-
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
 import com.bioinformaticsapp.data.BLASTQueryController;
 import com.bioinformaticsapp.data.BLASTQueryLabBook;
 import com.bioinformaticsapp.models.BLASTQuery;
-import com.bioinformaticsapp.models.BLASTQuery.Status;
-import com.bioinformaticsapp.models.BLASTVendor;
+import static com.bioinformaticsapp.models.BLASTQuery.Status.*;
+import static com.bioinformaticsapp.test.testhelpers.BLASTQueryBuilder.*;
 import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
 
 public class RetrievingLegacyQueriesTest extends InstrumentationTestCase {
@@ -24,13 +25,13 @@ public class RetrievingLegacyQueriesTest extends InstrumentationTestCase {
 		BLASTQueryController controller = new BLASTQueryController(getInstrumentation().getTargetContext());
 		List<BLASTQuery> runningQueries = controller.getSubmittedBLASTQueries();
 		controller.close();
-		assertEquals("Should return 'RUNNING' queries as 'SUBMITTED' ones", 1, runningQueries.size());
-		assertEquals("RUNNING queries should become SUBMITTED", Status.SUBMITTED, runningQueries.get(0).getStatus());
+		assertThat("Should return 'RUNNING' queries as 'SUBMITTED' ones", runningQueries.size(), is(1));
+		assertThat("RUNNING queries should become SUBMITTED", runningQueries.get(0).getStatus(), is(SUBMITTED));
 	}
 	
 	private void insertRunningBLASTQuery(){
-		BLASTQuery sampleQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		sampleQuery.setStatus(Status.RUNNING);
+		BLASTQuery sampleQuery = aBLASTQuery();
+		sampleQuery.setStatus(RUNNING);
 		sampleQuery.setJobIdentifier("ncbiblast-R20120418-133731-0240-81389354-pg");
 		BLASTQueryLabBook labBook = new BLASTQueryLabBook(getInstrumentation().getTargetContext());
 		labBook.save(sampleQuery);
