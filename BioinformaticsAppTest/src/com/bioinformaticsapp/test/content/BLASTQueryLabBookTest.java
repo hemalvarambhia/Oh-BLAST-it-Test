2036d1f9e39832dbeb07eaca84b0a15dbceecbb1
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-08-23 18:25:11 +0100

    Moved the Test class to the corresponding test package

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/content/BLASTQueryLabBookTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/content/BLASTQueryLabBookTest.java
@@ -1,4 +1,4 @@
-package com.bioinformaticsapp.test.functional;
+package com.bioinformaticsapp.test.content;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
