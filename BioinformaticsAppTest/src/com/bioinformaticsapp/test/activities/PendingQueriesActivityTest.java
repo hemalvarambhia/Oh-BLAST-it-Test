0b4ae032f4bb0e96f269b318fcab52626595ac86
Hemal Varambhia <h.n.varambhia@gmail.com>
2014-09-06 16:40:59 +0100

    Cleaned up the test

--- BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/PendingQueriesActivityTest.java
+++ BioinformaticsAppTest/src/com/bioinformaticsapp/test/activities/PendingQueriesActivityTest.java
@@ -1,33 +1,30 @@
 package com.bioinformaticsapp.test.activities;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.bioinformaticsapp.ViewBLASTQuerySearchParameters;
 import com.bioinformaticsapp.ListPendingBLASTQueries;
 import com.bioinformaticsapp.R;
+import com.bioinformaticsapp.ViewBLASTQuerySearchParameters;
 import com.bioinformaticsapp.content.BLASTQueryLabBook;
 import com.bioinformaticsapp.domain.BLASTQuery;
-import com.bioinformaticsapp.domain.BLASTVendor;
-import com.bioinformaticsapp.domain.SearchParameter;
 import com.bioinformaticsapp.domain.BLASTQuery.Status;
-import com.bioinformaticsapp.persistence.BLASTQueryController;
-import com.bioinformaticsapp.persistence.SearchParameterController;
+import com.bioinformaticsapp.test.testhelpers.BLASTQueryBuilder;
 import com.bioinformaticsapp.test.testhelpers.OhBLASTItTestHelper;
 import com.jayway.android.robotium.solo.Solo;
 
 public class PendingQueriesActivityTest extends
 		ActivityInstrumentationTestCase2<ListPendingBLASTQueries> {
-
-	
-	private static final String TAG = "PendingQueriesActivityTest";
 	
 	private Solo solo;
+	private BLASTQuery query;
 	
 	public PendingQueriesActivityTest(){
 		super(ListPendingBLASTQueries.class);
@@ -36,161 +33,62 @@ public class PendingQueriesActivityTest extends
 	public void setUp() throws Exception{
 		super.setUp();
 		OhBLASTItTestHelper helper = new OhBLASTItTestHelper(getInstrumentation().getTargetContext());
-		helper.cleanDatabase();
-		
+		helper.cleanDatabase();		
+		query = BLASTQueryBuilder.aBLASTQueryWithStatus(Status.SUBMITTED);
+		query.setJobIdentifier("ncbiblast-R20120418-133731-0240-81389354-pg");		
+		helper.save(query);
+		solo = new Solo(getInstrumentation(), getActivity());
+		solo.waitForView(ListView.class);
+		solo.waitForView(TextView.class);
 	}
 	
 	public void tearDown() throws Exception {
-		
 		solo.finishOpenedActivities();
-		
 		super.tearDown();
 	}
 	
 	public void testThatLongTappingAPendingQueryShowsUserAViewParametersOption(){
-		
-		BLASTQuery query = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		query.setStatus(Status.SUBMITTED);
-		query.setJobIdentifier("ncbiblast-R20120418-133731-0240-81389354-pg");		
-		query.setSearchParameter("email", "example@email.com");
-		saveQuery(query);
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.waitForView(TextView.class);
-		
 		solo.clickLongInList(1);
 		
 		String viewParametersOption = getInstrumentation().getTargetContext().getResources().getString(R.string.view_query_parameters);
-		
 		boolean menuItemShown = solo.searchText(viewParametersOption);
-		
-		assertTrue("Context menu with item allowing user to see parameters", menuItemShown);
-	
+		assertThat("Context menu with item allowing user to see parameters", menuItemShown);
 	}
 	
 	public void testThatWeCanSeeTheParametersOfSelectedEMBLQuery(){	
-		BLASTQuery emblQuery = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		emblQuery.setStatus(Status.SUBMITTED);
-		emblQuery.setJobIdentifier("ncbiblast-R20120418-133731-0240-81389354-pg");		
-		emblQuery.setSearchParameter("email", "example@email.com");
-		saveQuery(emblQuery);
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.waitForView(TextView.class);
-		
 		solo.clickLongInList(1);
-		
 		String viewParametersOption = getInstrumentation().getTargetContext().getResources().getString(R.string.view_query_parameters);
-		
 		solo.clickOnText(viewParametersOption);
 		
 		solo.assertCurrentActivity("Shows the search parameters activity", ViewBLASTQuerySearchParameters.class);
-		
-		boolean checkJobIdIsShownAsTitle = solo.searchText(emblQuery.getJobIdentifier());
-		
-		assertTrue("Title is the job identifier", checkJobIdIsShownAsTitle);
-		
-			
-	}
-	
-	public void testThatWeCanSeeTheParametersOfSelectedNCBIQuery(){
-		BLASTQuery ncbiQuery = new BLASTQuery("blastn", BLASTVendor.NCBI);
-		ncbiQuery.setStatus(Status.SUBMITTED);
-		ncbiQuery.setJobIdentifier("YAMJ8623016");		
-		saveQuery(ncbiQuery);
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.waitForView(TextView.class);
-		
-		solo.clickLongInList(1);
-		
-		String viewParametersOption = getInstrumentation().getTargetContext().getResources().getString(R.string.view_query_parameters);
-		
-		solo.clickOnText(viewParametersOption);
-		
-		solo.assertCurrentActivity("Shows the search parameters activity", ViewBLASTQuerySearchParameters.class);
-		
-		boolean checkJobIdIsShownAsTitle = solo.searchText(ncbiQuery.getJobIdentifier());
-		
-		assertTrue("Title is the job identifier", checkJobIdIsShownAsTitle);
-			
+		boolean jobIdIsShownAsTitle = solo.searchText(query.getJobIdentifier());
+		assertThat("Title is the job identifier", jobIdIsShownAsTitle);
 	}
 	
 	public void testWeCanDeleteABLASTQuery() throws IOException{
-		
-		BLASTQuery query = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		query.setStatus(Status.SUBMITTED);
-		query.setJobIdentifier("ncbiblast-R20120418-133731-0240-81389354-pg");		
-		query.setSearchParameter("email", "example@email.com");
-		saveQuery(query);
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.waitForView(TextView.class);
-		
 		solo.clickLongInList(1);
-		
-		String delete = "Delete";
-		
-		solo.clickOnText(delete);
-		
+		solo.clickOnText("Delete");
 		solo.clickOnText("OK");
 		solo.waitForView(ListView.class);
 		
-		ArrayList<ListView> listViews = solo.getCurrentListViews();
-		
-		for(ListView listView : listViews){
-			
-			assertEquals("Should be able to delete a query", 0, listView.getAdapter().getCount());
-			
-		}
-	}
-	
-	public void testWeCanCancelADelete() throws IOException{
-		
-		BLASTQuery query = new BLASTQuery("blastn", BLASTVendor.EMBL_EBI);
-		query.setStatus(Status.SUBMITTED);
-		query.setJobIdentifier("ncbiblast-R20120418-133731-0240-81389354-pg");		
-		query.setSearchParameter("email", "example@email.com");
-		saveQuery(query);
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo = new Solo(getInstrumentation(), getActivity());
-		
-		solo.waitForView(TextView.class);
-		
-		solo.clickLongInList(1);
-		
-		String delete = "Delete";
-		
-		solo.clickOnText(delete);
-		
-		solo.clickOnText("Cancel");
-		
-		ArrayList<ListView> listViews = solo.getCurrentListViews();
-		BLASTQueryController queryController = new BLASTQueryController(getInstrumentation().getTargetContext());
-		List<BLASTQuery> finishedQueries = queryController.getSubmittedBLASTQueries();
-		queryController.close();
-		
-		for(ListView listView : listViews){
-			
-			assertEquals("Cancellation should not delete the query", finishedQueries.size(), listView.getAdapter().getCount());
-			
-		}
-		
+		ListView listView = listOfBLASTQueries();
+		assertThat("Should be able to delete a query", listView.getAdapter().isEmpty());
 	}
 
-	private long saveQuery(BLASTQuery query){
+	public void testWeCanCancelADelete() throws IOException{
+		solo.clickLongInList(1);
+		String delete = "Delete";
+		solo.clickOnText(delete);
+		solo.clickOnText("Cancel");
+		solo.waitForView(ListView.class);
+		
+		ListView listView = listOfBLASTQueries();
 		BLASTQueryLabBook labBook = new BLASTQueryLabBook(getInstrumentation().getTargetContext());
-		BLASTQuery saved = labBook.save(query);
-		return saved.getPrimaryKey();
+		List<BLASTQuery> finishedQueries = labBook.findBLASTQueriesByStatus(Status.SUBMITTED);
+		assertThat("Cancellation should not delete the query", listView.getAdapter().getCount(), is(finishedQueries.size()));
 	}
 	
+	private ListView listOfBLASTQueries() {
+		return solo.getCurrentListViews().get(0);
+	}
 }
